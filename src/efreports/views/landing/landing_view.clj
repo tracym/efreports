f13ba6a1927c374e4d62add5670910d848ea840a
Tracy Malkemes <tuz24919@temple.edu>
2014-02-12 15:34:42 -0500

    make the landing page text suck less

--- src/efreports/views/landing/landing_view.clj
+++ src/efreports/views/landing/landing_view.clj
@@ -17,50 +17,41 @@
    [:div {:class "jumbotron"}
     [:div {:class"container"}
      [:h2 "Fast, Dynamic Data Manipulation in the Browser"]
-     [:p "Ef Reports is an application that allows users without knowledge of SQL to sort, filter, group, and join data in the browser using a clean, modern
-      interface built with Bootstrap. Sign in to demo the software or watch the screencast to see if Ef Reports would be right for you."]
+     [:p "Ef Reports let's you sort, filter, group, and join data in the browser. Sign in to give it a try."]
      [:p [:a {:class "btn btn-primary btn-lg" :role "button" :href "/login"} "Sign In &raquo;"]]
      ]]
    [:div {:class "container"}
     [:div {:class "row"}
      [:div {:class "col-md-4"}
       [:h3 "A Thought Experiment"]
-      [:p "If the end users of enterprise reporting software had the ability to quickly manipulate data in a way similar to report developers, would enterprise reports be necessary?
-           After all, if the goal of a report is to present information from a data source to people who want to know, why must this be accomplished
-           with endless, mind-numbing \"Report Design\" widgets and all the other trappings of enterprise software? Are end users really satisfied as passive consumers of information
-           when they could be empowered to manipulate pre-defined collections of data to find the information they want when they want it?
+      [:p "If anyone could manipulate data like report developers, would enterprise reports be necessary? Ef Reports allows users to slice up their data any way they want and see the results in seconds.
        "]
       ]
-     [:div {:class "col-md-4"}
-      [:h3 "No Hassles - For Anybody"]
-      [:p "Let administrators create collections using only SQL. Let end users manipulate them and save the results. The results of any series of manipulations can be downloaded into an Excel file at any time."]
-      ]
+
      [:div {:class "col-md-4"}
       [:h3 "Features"]
          [:ul
           [:li "Sort, filter, group, and join data in the browser"]
           [:li "Written in Clojure for performance and scalability"]
-          [:li "Data is only pulled from the database on a user's first accessing a collection or when "]
+          [:li "Users determine when to pull data from the database"]
+          [:li "All data is manipulated in memory"]
           ]
 
      ]
      [:div {:class "col-md-4"}
       [:h3 "Roadmap"]
-      [:p "This is still very much a side project and the purpose of this demo app is to guage what kind of interest is out there for a tool of this sort. The amount of interest (if any) and
-         and the feedback I receive will determine how I move forward with this. Should this be free software or should I trash it? Let me know!
-         However, there are a few features I would like to add before any kind of rollout. Among them:"]
-      [:ul
-       [:li "Allow for multiple datasources such that each collection can have its own datasource (this could be another db or csv file)"]
-       ]
+      [:p "Ef Reports is very much a side project and the purpose of this demo is to guage what kind of interest there is for a tool like this. The amount of interest (if any) and
+         and the feedback I receive will determine how I move forward with this. Should this be free software or should I trash it? Let me know!"]
      ]
      [:div {:class "col-md-4"}
       [:h3 "Known Issues"]
       [:ul
-       [:li [:strong "Forms that accept direct SQL input should be moved or removed because security"]]
+       [:li "Forms that accept direct SQL input should be moved or removed because security"]
        [:li "The name of this project probably should be changed"]
-
-
-
+       [:li "Joining or \"mapping\" data does not always behave as one would expect"]
+       [:li "Pivot tables can only be generated from Collections that have not been manipulated"]
+       [:li "Nulls and data types other than numbers and strings are not handled reliably. When creating Collections, liberal use of CAST and COAELESCE are your best bet"]
+       [:li "Mapping result sets causes noticeable performance degredation."]
        ]
       ]
 
@@ -69,4 +60,4 @@
 )
 
 (defn landing-page []
-  (layout/common "Welcome" nil  (landing-html)))
+  (layout/common "Welcome to Ef Reports" nil  (landing-html)))
