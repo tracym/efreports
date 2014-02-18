859c3051668b369d2f492acf396483dc33e5ba3c
Tracy Malkemes <tuz24919@temple.edu>
2014-02-18 14:07:13 -0500

    updated landing page

--- src/efreports/views/landing/landing_view.clj
+++ src/efreports/views/landing/landing_view.clj
@@ -41,20 +41,23 @@
      [:div {:class "col-md-4"}
       [:h3 "Roadmap"]
       [:p "Ef Reports is very much a side project and the purpose of this demo is to guage what kind of interest there is for a tool like this. The amount of interest (if any) and
-         and the feedback I receive will determine how I move forward with this. Should this be free software or should I trash it? Let me know!"]
+         and the feedback I receive will determine how I move forward with this. Should this be free software or should I trash it?" [:a {:href "mailto:efreportsdemo@gmail.com"} "Let me know!"]]
      ]
      [:div {:class "col-md-4"}
       [:h3 "Known Issues"]
       [:ul
        [:li "Forms that accept direct SQL input should be moved or removed because security"]
        [:li "The name of this project probably should be changed"]
-       [:li "Joining or \"mapping\" data does not always behave as one would expect"]
+       [:li "Joining or \"mapping\" data behaves more like a left join than an inner join"]
        [:li "Pivot tables can only be generated from Collections that have not been manipulated"]
        [:li "Nulls and data types other than numbers and strings are not handled reliably. When creating Collections, liberal use of CAST and COAELESCE are your best bet"]
-       [:li "Mapping result sets causes noticeable performance degredation."]
+       [:li "Mapping result sets causes noticeable performance degredation"]
        ]
       ]
-
+     [:div {:class "col-md-4"}
+      [:h3 "Feedback"]
+      [:p "Email " [:a {:href "mailto:efreportsdemo@gmail.com"} "efreportsdemo@gmail.com."] " Your feedback is important."]
+      ]
     ]]
 
 )
